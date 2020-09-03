ac9705d0a5e42ad737b6c69a4dca42803f21819c
nahjh2 <nahjh2@google.com>
2020-09-03 14:05:38 +0900

    cupertino, material design

---
+++ lib/cupertino_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CupertinoPage extends StatefulWidget {
+  @override
+  _CupertinoPageState createState() => _CupertinoPageState();
+}
+
+class _CupertinoPageState extends State<CupertinoPage> {
+  bool _switch = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text("쿠퍼티노 UI"),
+      ),
+      body:Column(
+        children: [
+          Text("쿠퍼티노"),
+          CupertinoButton(
+            child: Text("쿠퍼티노 버튼"),
+          ),
+          CupertinoSwitch(
+            value: _switch,
+            onChanged: (bool value) {
+              setState(() {
+                _switch = value;
+              });
+            },
+          ),
+          Text("머티리얼"),
+          RaisedButton(
+            child: Text("머티리얼 버튼"),
+          ),
+          Switch(
+            value: _switch,
+            onChanged: (bool value) {
+              setState(() {
+                _switch = value;
+              });
+            },
+          )
+        ],
+      )
+    );
+  }
+}
+
